692e95451a3c0722cb24a04e96355d3a9c335892
amgoodfellow <amgoodfellow@oakland.edu>
2017-10-12 18:47:38 -0400

    Necessary for local jwt for now

--- src/main/java/edu/oakland/ideas/IdeasApplication.java
+++ src/main/java/edu/oakland/ideas/IdeasApplication.java
@@ -3,9 +3,11 @@ package edu.oakland.ideas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.apereo.portal.soffit.renderer.SoffitApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SoffitApplication
 @SpringBootApplication
+@ComponentScan(basePackages = {"edu.oakland"})
 public class IdeasApplication {
 
 	public static void main(String[] args) {
